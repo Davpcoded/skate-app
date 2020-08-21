06d17ac0d028b30707427e74e87f0e5f54c4cf4a
Daniel Villarroel <handonsk82894@gmail.com>
2020-08-21 12:32:06 -0700

    modified package.json to have proper dependencies

--- server.js
+++ server.js
@@ -1,51 +1,24 @@
 const express = require("express");
-const path = require("path");
-const cors = require("cors");
-//const passport = require("passport");
-//const passportLocal = require("passport-local").Strategy;
-const cookieParser = require("cookie-parser");
-const bcrypt = require("bcryptjs");
-const session = require("express-session");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
 const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/skate-app", {
-  useNewUrlParser: true,
-});
+//const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
-app.use(
-  session({
-    resave: true,
-    secret: "secretcode",
-    saveUninitialized: false,
-  })
-);
-
-/* app.use(passport.initialize());
-app.use(passport.session());
-require("./config/passport"); */
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+//app.use(routes);
 
-// Define API routes here
-/* const apiRoutes = require("./routes/api.js");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/skate-app");
 
-app.use("/api", apiRoutes);
- */
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
