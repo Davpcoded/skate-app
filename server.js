bb684b8d840944dbd367ae650857606928c4d29a
Daniel Villarroel <handonsk82894@gmail.com>
2020-08-20 22:03:05 -0700

    created and organized folder structure

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+//const passport = require("passport");
+//const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/skate-app", {
+  useNewUrlParser: true,
+});
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    resave: true,
+    secret: "secretcode",
+    saveUninitialized: false,
+  })
+);
+
+/* app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport"); */
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+/* const apiRoutes = require("./routes/api.js");
+
+app.use("/api", apiRoutes);
+ */
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
