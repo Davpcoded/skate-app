67dc8113b8e535dd589a402949dde8b0c339f15c
Daniel Villarroel <handonsk82894@gmail.com>
2020-08-27 23:45:33 -0700

    added custom components to style map

---
+++ client/src/components/MapStyles/styles.js
@@ -0,0 +1,116 @@
+export default [
+  {
+    featureType: "all",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#a8afbf",
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        visibility: "on",
+      },
+      {
+        color: "#373d48",
+      },
+      {
+        weight: 2,
+      },
+      {
+        gamma: "1",
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        weight: 0.6,
+      },
+      {
+        color: "#4c576f",
+      },
+      {
+        gamma: "0",
+      },
+    ],
+  },
+  {
+    featureType: "landscape",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#424c65",
+      },
+      {
+        gamma: "1",
+      },
+      {
+        weight: "10",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#4c576f",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#424d66",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#37425c",
+      },
+      {
+        lightness: "0",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#4c576f",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2b364f",
+      },
+    ],
+  },
+];
--- client/src/pages/ShareSpot.js
+++ client/src/pages/ShareSpot.js
@@ -1,32 +1,92 @@
 import React from "react";
-/* import ReactDOM from "react-dom"; */
-import Button from "@material-ui/core/Button";
 import {
-  withScriptjs,
-  withGoogleMap,
   GoogleMap,
+  useLoadScript,
   Marker,
-} from "react-google-maps";
+  InfoWindow,
+} from "@react-google-maps/api";
+import styles from "../components/MapStyles/styles";
 
-function ShareSpot() {
-  const MapWithAMarker = withScriptjs(
-    withGoogleMap((props) => (
-      <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: 47.6061, lng: -122.3321 }}
-      >
-        <Marker position={{ lat: 47.6061, lng: -122.3321 }} />
-      </GoogleMap>
-    ))
-  );
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "50vh",
+  width: "100vw",
+};
+const options = {
+  styles: styles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 47.6061,
+  lng: -122.3321,
+};
+
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyC8DUc5piT5BTqIsONJKHMOyT9-LLtAVwg",
+    libraries,
+  });
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
   return (
-    <MapWithAMarker
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC8DUc5piT5BTqIsONJKHMOyT9-LLtAVwg&callback=initMap"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
+    <div>
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      ></GoogleMap>
+      <Locate panTo={panTo} />
+    </div>
   );
 }
 
-export default ShareSpot;
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="/compass.svg" alt="compass" />
+    </button>
+  );
+}
