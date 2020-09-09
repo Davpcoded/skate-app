704a1fdb813ad31978887918907404ec93d4b890
Daniel Villarroel <handonsk82894@gmail.com>
2020-09-09 11:49:43 -0700

    Testing deployment in heroku

--- routes/HTML-Routes.js
+++ routes/HTML-Routes.js
@@ -1,3 +1,7 @@
-router.get("*", function (req, res) {
+const router = require("express").Router();
+const db = require("../models");
+const { Error } = require("mongoose");
+
+router.get("/sharespot", function (req, res) {
   res.send("hello world");
 });
