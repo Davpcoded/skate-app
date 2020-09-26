1dc33fcbed44195cb6221ad853099514a2731edd
Daniel Villarroel <handonsk82894@gmail.com>
2020-09-26 14:27:35 -0700

    developing Home page UI / UX

--- client/src/components/AboutSection/index.js
+++ client/src/components/AboutSection/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import IconButton from "@material-ui/core/IconButton";
-import InfoIcon from "@material-ui/icons/Info";
+import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
+    marginTop: "8px",
   },
   image: {
     backgroundImage: "url(https://source.unsplash.com/NDlg6cW1Yzk)",
@@ -29,14 +26,42 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     color: "rgba(255, 255, 255, 0.54)",
   },
+  bannerText: {
+    color: "white",
+    width: "350px",
+    borderRadius: "0.5rem",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderStyle: "ridge",
+    marginTop: "20vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 }));
 
 export default function AboutSection() {
   const classes = useStyles();
   return (
-    <Grid container component="main" className={classes.root}>
+    <Grid container className={classes.root}>
       <CssBaseline />
-      <Grid item xs={12} className={classes.image} />
+      <Grid
+        item
+        xs={12}
+        elevation={6}
+        square
+        component={Paper}
+        className={classes.image}
+      >
+        <Typography
+          xs={12}
+          className={classes.bannerText}
+          variant="h4"
+          component="h4"
+        >
+          Skate Nearby spots
+        </Typography>
+      </Grid>
     </Grid>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -91,7 +91,16 @@ export default function SignInSide() {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        component={Paper}
+        elevation={6}
+        square
+        className={classes.image}
+      />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
