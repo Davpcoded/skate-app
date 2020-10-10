ea8f255967db5c052b86c200af0d1763b3a40160
Daniel Villarroel <handonsk82894@gmail.com>
2020-10-10 11:29:41 -0700

    added responsive appBar with Drawer

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,23 +1,13 @@
 import React from "react";
 import clsx from "clsx";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
 import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
-//==============icons================//
-import InstagramIcon from "@material-ui/icons/Instagram";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import YouTubeIcon from "@material-ui/icons/YouTube";
-import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
-import OndemandVideoOutlinedIcon from "@material-ui/icons/OndemandVideoOutlined";
-import RoomOutlinedIcon from "@material-ui/icons/RoomOutlined";
-import AddLocationOutlinedIcon from "@material-ui/icons/AddLocationOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
@@ -25,39 +15,70 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: "flex",
+  },
+  appBar: {
+    backgroundColor: "transparent",
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
   },
-  navButton: {
-    marginRight: theme.spacing(1),
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
+  hide: {
+    display: "none",
   },
-  navIcon: {
-    marginRight: theme.spacing(3),
-    [theme.breakpoints.down("sm")]: {
-      marginRight: theme.spacing(0),
-      padding: "0",
-    },
-    color: "white",
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
   },
-  title: {
-    alignContent: "center",
-    marginLeft: "auto",
-    marginRight: "auto",
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
-export default function ButtonAppBar() {
+export default function PersistentDrawerLeft() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -72,65 +93,72 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
+      <CssBaseline />
       <AppBar
-        style={{ background: "transparent" }}
+        position="fixed"
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
-        position="static"
       >
         <Toolbar>
-          <Button className={classes.navIcon} href="/sharespot">
-            <InstagramIcon className={classes.navIcon} />
-          </Button>
-          <Button className={classes.navIcon}>
-            <FacebookIcon className={classes.navIcon} />
-          </Button>
-          <Button className={classes.navIcon}>
-            <YouTubeIcon className={classes.navIcon} />
-          </Button>
-
-          <Typography variant="h6" className={classes.title}>
-            SpotShredder
-          </Typography>
-
-          <Button
-            className={classes.navButton}
-            href="/sharespot"
-            color="inherit"
-          >
-            Videos
-          </Button>
-          <Button
-            className={classes.navButton}
-            href="/sharespot"
-            color="inherit"
-          >
-            Add Spot
-          </Button>
-          <Button
-            className={classes.navButton}
-            href="/sharespot"
-            color="inherit"
-          >
-            Spots
-          </Button>
-
           <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
             edge="start"
             className={clsx(classes.menuButton, open && classes.hide)}
-            onClick={handleDrawerOpen}
-            color="inherit"
-            aria-label="menu"
           >
             <MenuIcon />
           </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 }
-
 {
   /* <List component="nav" aria-label="main mailbox folders">
           <ListItem className={classes.gutters} button>
